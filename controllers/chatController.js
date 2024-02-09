8846c4fc5c5b01493e13948871c0ded7312ee331
Shreshth Bansal <shreshthbansal2505@gmail.com>
2024-02-09 14:06:04 +0530

    Code updated

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,7 +4,6 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 const chat = async(req, resp)=>{
     const chats = req.body.chat
-    console.log(chats) 
     const completion = await openai.chat.completions.create({
         messages: chats,
         model: "gpt-3.5-turbo",
@@ -19,7 +18,6 @@ const analyzeEmotion = async(req, resp)=> {
   try {
       const text = req.body.text
       const content = `What emotion is the following text expressing?\n${text}`;
-      console.log(content)
       const response = await openai.chat.completions.create({
           model: "gpt-3.5-turbo-16k",
           messages: [
@@ -37,5 +35,27 @@ const analyzeEmotion = async(req, resp)=> {
   }
 }
 
+const analyzeStory = async(req, resp)=> {
+    try {
+        const narrative_style = 'linear narrative'
+        const text = req.body.text
+        const content = `Turn the following transcript into an engaging story with real-world examples and ensure the content in bullet points.The narrative style should be a ${narrative_style}:\n${text}`
 
-module.exports = {analyzeEmotion, chat}
+        const messages = [{"role": "system", "content": "You are a helpful assistant that can convert transcripts into engaging stories with characters and real-world examples."}, {"role": "user", "content": content}]
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-16k",
+            messages: messages,
+            max_tokens: 10000
+        });
+        const emotion = response.choices[0].message.content;
+        return resp.json({"message":emotion.trim()});
+    } catch (error) {
+        console.log(error)
+        console.error(`${new Date()} [ERROR]: Error in emotion analysis: ${error}`);
+        return null;
+    }
+  }
+
+
+module.exports = {analyzeEmotion, chat, analyzeStory}
--- routes/chat.js
+++ routes/chat.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {chat, analyzeEmotion} = require('../controllers/chatController')
+const {chat, analyzeEmotion, analyzeStory} = require('../controllers/chatController')
 
 const chatRouter = express.Router()
 
 chatRouter.post('', chat)
 chatRouter.post('/analyze', analyzeEmotion)
+chatRouter.post('/story', analyzeStory)
 
 
 module.exports = chatRouter
